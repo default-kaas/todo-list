8b36bb3770e7237cbcdc2efcfa2c43576c2d6d73
default-kaas <dogen1996@live.nl>
2023-08-22 20:32:23 +0200

    Initial commit

---
+++ components/checkListAsItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLink :to="`/check-list/${id}`">
+    <h2>
+      {{ title }}
+    </h2>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  id: number,
+  title: string
+}>(), {})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline bg-green-700">
+      Check lists:
+    </h1>
+    <template v-for="checkList in checkLists.checkLists" :key="checkList.id">
+      <CheckListAsItem :id="checkList.id" :title="checkList.name" />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+const checkLists = {
+  checkLists: [
+    {
+      id: 1,
+      name: 'test1',
+      items: [{
+        id: 1,
+        checked: false,
+        text: '',
+        screenshotIds: [],
+        links: ''
+      }]
+    },
+    {
+      id: 2,
+      name: 'test2',
+      items: [{
+        id: 1,
+        checked: false,
+        text: '',
+        screenshotIds: [],
+        links: ''
+      }]
+    },
+    {
+      id: 3,
+      name: 'test3',
+      items: [{
+        id: 1,
+        checked: false,
+        text: '',
+        screenshotIds: [],
+        links: ''
+      }]
+    },
+  ]
+}
+</script>
