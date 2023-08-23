826d005a6bf40b67e7ac91363ccb2c3dca4e5181
default-kaas <dogen1996@live.nl>
2023-08-23 22:39:12 +0200

    Improved styling part 1 of 9999

--- components/checkListAsItem.vue
+++ components/checkListAsItem.vue
@@ -1,14 +1,26 @@
 <template>
-  <NuxtLink :to="`/check-list/${id}`">
-    <h2>
-      {{ title }}
-    </h2>
-  </NuxtLink>
+  <div class="mx-1 my-2">
+    <!-- TODO: figure out why tailwind styling does not apply on NuxtLink -->
+    <NuxtLink :to="`/check-list/${id}`">
+      <div class="px-2 py-3 border border-gray-300 shadow-md rounded">
+        <h2 class="font-bold">
+          {{ title }}
+        </h2>
+        <ul v-if="collaborators.length > 0" class="flex ml-1 font-medium text-emerald-500">
+          <li v-for="collaborator in collaborators" class="mx-1">
+            @{{ collaborator }}
+          </li>
+        </ul>
+        <div class="ml-2 font-medium" v-else>-</div>
+      </div>
+    </NuxtLink>
+  </div>
 </template>
 
 <script setup lang="ts">
 withDefaults(defineProps<{
   id: number,
   title: string
+  collaborators: string[]
 }>(), {})
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline bg-green-700">
-      Check lists:
+    <h1 class="text-3xl font-bold pl-1 py-2 bg-emerald-600">
+      Check lists
     </h1>
+    <!-- TODO: add search on check list name -->
     <template v-for="checkList in checkLists.checkLists" :key="checkList.id">
-      <CheckListAsItem :id="checkList.id" :title="checkList.name" />
+      <CheckListAsItem :id="checkList.id" :title="checkList.name" :collaborators="checkList.collaborators" />
     </template>
   </div>
 </template>
